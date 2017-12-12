cdc392b8b203092e4a40af396cd49e6193dc9a45
Elliot Tormey <elliot.tormey@tapadoo.com>
2017-12-12 10:15:13 +0000

    use avdmanager instead of deprecated tool

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 
-echo no | android create avd --force -n $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME -t $WERCKER_SETUP_ANDROID_EMULATOR_TARGET --abi $WERCKER_SETUP_ANDROID_EMULATOR_ABI
+echo no | avdmanager create avd -f -n $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME -k $WERCKER_SETUP_ANDROID_EMULATOR_TARGET --abi $WERCKER_SETUP_ANDROID_EMULATOR_ABI
 echo "hw.keyboard=yes" >> ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME.avd/config.ini
 echo "Enabled hardware keyboard"
 cat ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME.avd/config.ini
